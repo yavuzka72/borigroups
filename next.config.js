1c1e2c24f4f5827edb9f8dd9a4166ea10dfa03d9
yavuzka72 <61251673+yavuzka72@users.noreply.github.com>
2022-10-14 15:03:40 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,6 +3,6 @@ module.exports = {
     trailingSlash: true,
     env: {
         PUBLIC_URL: process.env.NODE_ENV === 'production' ? `/react/molla/demo-7/` : '/',
-        APP_URL: process.env.NODE_ENV === 'production' ? 'https://borigroups-qcmhd9w6f-yavuzka72.vercel.app/react/molla/demo-7/' : 'http://localhost/'
+        APP_URL: process.env.NODE_ENV === 'production' ? 'https://borigroups-yavuzka72.vercel.app/react/molla/demo-7/' : 'http://localhost/'
     }
 }
